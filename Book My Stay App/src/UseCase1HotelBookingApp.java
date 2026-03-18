0f9f9712e059e34b9e3e344028ba4f3b1c4e48eb
darun000 <reachoutdarun@gmail.com>
2026-03-18 09:49:44 +0530

    UC1 completed - Application entry and welcome message

--- Book My Stay App/src/UseCase1HotelBookingApp.java
+++ Book My Stay App/src/UseCase1HotelBookingApp.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+public class UseCase1HotelBookingApp {public static void main(String[] args) {
+    String appName = "Book My Stay";
+    String version = "v1.0";
 
-public class UseCase1HotelBookingApp {
+    System.out.println("Welcome to " + appName);
+    System.out.println("Hotel Booking Management System " + version);
+}
 }
